fe93494875cd3bbd77b7bda0ce9cd074f35f0e12
Sahil Pahuja <sahilpahuja9632@gmail.com>
2022-03-10 09:45:31 +0530

    added Sender and Receiver relationship

--- app/Models/Receiver.php
+++ app/Models/Receiver.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Receiver extends Model
 {
     use HasFactory;
 
     protected $table = 'users';
+
+    public function message(): HasMany
+    {
+        return $this->hasMany(Message::class, 'receiver_id');
+    }
 }
